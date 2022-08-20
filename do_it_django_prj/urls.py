8d8a4f58ea23350e86f78ea7b751a515ba17860e
eunji <mol39ra@naver.com>
2022-08-21 03:49:11 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
+
 ]

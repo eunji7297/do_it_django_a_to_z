c67c918d46a2caa63264c5ce90c4d5fe8a69dfe5
eunji <mol39ra@naver.com>
2022-08-23 23:56:39 +0900

    포스트 목록 페이지와 포스트 상세 페이지 디자인 통일하기

--- blog/tests.py
+++ blog/tests.py
@@ -64,7 +64,7 @@ class TestView(TestCase):
         #2.1 첫 번째 포스트의 url로 접근하면 정상적으로 작동한다
         response = self.client.get(post_001.get_absolute_url())
         self.assertEqual(response.status_code,200)
-        soup=BeautifulSoup(response.content,'html.parser')
+        soup = BeautifulSoup(response.content,'html.parser')
 
         #2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
         navbar = soup.nav
@@ -77,11 +77,13 @@ class TestView(TestCase):
         #2.4 첫 번째 포스트의 제목이 포스트 영역에 있다.
         main_area = soup.find('div', id='main-area')
         post_area = main_area.find('div', id='post-area')
-        self.assertIn(post_001, post_area.text)
+        self.assertIn(post_001.title, post_area.text)
 
         #2.5 첫 번째 포스트의 작성자가 포스트 영역에 있다.
         #2.6 첫 번째 포스트의 내용이 포스트 영역에 있다.
         self.assertIn(post_001.content, post_area.text)
+
+
 
 
 
